0da54743017d2aa1cfa735eb61aad9a571fddefa
ALMA_DEV914 <aungonalna58@gamil.com>
2024-07-02 21:35:45 -0700

    styled and add the top bar menu

--- src/globalComponents/Topbar.jsx
+++ src/globalComponents/Topbar.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
-import LightModeOutlined from '@mui/icons-material/LightModeOutlined';
-import NotificationsOutlined from '@mui/icons-material/NotificationsOutlined';
-import SettingsOutlined from '@mui/icons-material/SettingsOutlined';
-import PersonOutlined from '@mui/icons-material/PersonOutlined';
-import Search from '@mui/icons-material/Search';
+
+import SearchIcon from '@mui/icons-material/Search';
 
 import { Box, IconButton, useTheme } from "@mui/material";
 import { useContext } from "react";
 import { ColorModeContext, tokens } from "../theme";
 import {InputBase} from "@mui/material";
+import { DarkModeOutlined, LightModeOutlined, NotificationAddOutlined, Person2Outlined, SettingsOutlined } from '@mui/icons-material';
 
 const Topbar = () => {
   const theme = useTheme();
@@ -16,13 +14,30 @@ const Topbar = () => {
   const colorMode = useContext(ColorModeContext);
   return <Box display="flex" justifyContent="space-between" p={2}>
     <Box display="flex" backgroundColor={colors.primary[400]} borderRadius="3px">
-    <InputBase></InputBase>
-     <Search></Search>
-     <LightModeOutlined></LightModeOutlined>
-     <SettingsOutlined></SettingsOutlined>
-     <PersonOutlined></PersonOutlined>
-     <NotificationsOutlined></NotificationsOutlined>
-     </Box>
+    <InputBase sx={{ml: 2, flex:1}} placeholder='Search'></InputBase>
+    
+     <IconButton type="button" sx={{ p: 1 }}>
+          <SearchIcon />
+        </IconButton>
+        </Box>
+        <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === "dark" ? (
+            <DarkModeOutlined />
+          ) : (
+            <LightModeOutlined/>
+          )}
+        </IconButton>
+        <IconButton>
+          <NotificationAddOutlined/>
+        </IconButton>
+        <IconButton>
+          <SettingsOutlined/>
+        </IconButton>
+        <IconButton>
+          <Person2Outlined />
+        </IconButton>
+      </Box>
   </Box>;
 };
 
