a36656f3f3c99ae3563c722cdfb3cb7639fd3e52
ALMA_DEV914 <aungonalna58@gamil.com>
2024-07-03 22:34:52 -0700

    add form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { CssBaseline, ThemeProvider } from "@mui/material";
-import {ColorModeContext, useMode } from "./theme";
+import { ColorModeContext, useMode } from "./theme";
 import Topbar from "./globalComponents/Topbar";
 import Sidebar from "./globalComponents/Sidebar";
 import { useState } from "react";
-
+import { Route, Routes } from "react-router-dom";
+import Form from "./scenes/form/form";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -11,20 +12,19 @@ function App() {
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
-    <CssBaseline>
-      <div className="app
-      ">
-         <Sidebar isSidebar={isSidebar} />
-        <main className="content">
-          <Topbar />
-
-          
-        </main>
-      </div>
-    </CssBaseline>
+        <CssBaseline>
+          <div
+            className="app">
+            <main className="content">
+              <Topbar setIsSidebar={setIsSidebar} />
+              <Routes>
+                <Route path="/form" element={<Form />} />
+              </Routes>
+            </main>
+          </div>
+        </CssBaseline>
       </ThemeProvider>
     </ColorModeContext.Provider>
-    
   );
 }
 
